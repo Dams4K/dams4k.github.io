e8cb265e640759573a9a4999ab571c6522f04f3e
Dams4K <dams4k@users.noreply.github.com>
2023-03-23 21:56:49 +0100

    Minesweeper + fmod

--- planet_informations.js
+++ planet_informations.js
@@ -1,3 +1,6 @@
+// adding fmod function
+Math.fmod = function (a,b) { return Number((a - (Math.floor(a / b) * b)).toPrecision(8)); };
+
 const planetDisplayNames = {
     "star": "This world",
     "nsikipedia": "NSIkipédia",
@@ -71,7 +74,7 @@ class Planet {
             var hitboxRect = this.planetHitbox.getBoundingClientRect();
             var centerX = (hitboxRect.left+hitboxRect.right)/2;
             var centerY = (hitboxRect.top+hitboxRect.bottom)/2;
-            console.log(vh2px(parseInt(this.imageSize.substr(0, this.imageSize.length-2))));
+
             this.label.style.top = `${centerY + vh2px(parseInt(this.imageSize.substr(0, this.imageSize.length-2)))/2 + this.textOffsetY}px`;
             this.label.style.left = `${centerX - this.label.offsetWidth/2}px`;
         }, 0.05);
@@ -86,7 +89,7 @@ var sun = new Planet("assets/textures/sun.gif", "20vh", "20vh", 0, "This World")
 sun.textOffsetY = -30;
 sun.showText(solarSystem);
 
-var cpsdisplay = new Planet("assets/textures/cpsdisplay.gif", "4vh", "40vh", 15000, "CPSDisplay");
+var cpsdisplay = new Planet("assets/textures/cpsdisplay.gif", "4vh", "30vh", 25000, "CPSDisplay");
 cpsdisplay.textOffsetY = 10;
 cpsdisplay.showText(solarSystem);
 
@@ -94,6 +97,12 @@ var nsikipedia = new Planet("assets/textures/nsikipedia.gif", "3vh", "20vh", 300
 nsikipedia.textOffsetY = 5;
 nsikipedia.showText(solarSystem);
 
+var minesweeper = new Planet("assets/textures/minesweeper.gif", "3.5vh", "20vh", 30000, "Minesweeper")
+minesweeper.textOffsetY = 5;
+minesweeper.initialRotation = Math.fmod(nsikipedia.initialRotation + 100, 360);
+minesweeper.showText(solarSystem);
+
 sun.addPlanet(solarSystem);
 cpsdisplay.addPlanet(solarSystem);
 nsikipedia.addPlanet(solarSystem);
+minesweeper.addPlanet(solarSystem);
