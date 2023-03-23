c77c53a8cb556b551c2c41d253319547987adc00
Dams4K <dams4k@users.noreply.github.com>
2023-03-23 15:26:24 +0100

    names for planets

--- planet_informations.js
+++ planet_informations.js
@@ -1,33 +1,35 @@
+const planetDisplayNames = {
+    "star": "This world",
+    "nsikipedia": "NSIkipédia",
+    "cpsdisplay": "CPSDisplay"
+};
+
 const planetInformationDiv = document.createElement("div");
 planetInformationDiv.style.position = "absolute";        
 document.body.appendChild(planetInformationDiv);
 
-const planets = document.getElementsByClassName("planet_hitbox");
+const planetHitBoxs = document.getElementsByClassName("planet_hitbox");
 var planetNames = {}
 
-for (const planet of planets) {
-    planetNames[planet.id] = document.createElement("div");
+for (const planetHitbox of planetHitBoxs) {
+    planet = planetHitbox.parentElement;
+    planetNames[planetHitbox.id] = document.createElement("div");
     
-    planetName = planetNames[planet.id];
-    planetName.innerText = "TEXT";
+    planetName = planetNames[planetHitbox.id];
+    planetName.innerText = planetDisplayNames[planet.id];
     planetName.style.position = "absolute";
     planetName.classList.add("planet_name");
 
-    setInterval(() => {followPlanet(planet);}, 0.05);
+    setInterval(() => {followPlanet(planetHitbox);}, 0.05);
     document.body.appendChild(planetName);
-    
-
-    // planet.addEventListener("mouseover", (event) => {
-    //     planetInformationDiv.innerText = `${event.clientX}, ${event.clientY}`;
-    //     followPlanet(planet);
-    // });
 }
 
-function followPlanet(planet) {
-    var element = planetNames[planet.id];
-    console.log(element.id);
-    var boudingPlanetRect = planet.getBoundingClientRect();
+function followPlanet(planetHitbox) {
+    var planet = planetHitbox.parentElement;
+    var element = planetNames[planetHitbox.id];
+    var boudingPlanetRect = planetHitbox.getBoundingClientRect();
+    var parentBoudingPlanetRect = planet.getBoundingClientRect();
     element.style.left = `${boudingPlanetRect.left}px`;
-    element.style.top = `${boudingPlanetRect.top+40}px`;
+    element.style.top = `${boudingPlanetRect.top + boudingPlanetRect.height}px`;
     element.style.width = `${boudingPlanetRect.width}px`;
 }
